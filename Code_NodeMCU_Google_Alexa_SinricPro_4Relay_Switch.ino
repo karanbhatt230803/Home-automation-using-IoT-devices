4ce07ca1fb8fa3756a61b847722693977b2a2c08
Yatharth Chauhan <yc232326@gmail.com>
2023-09-02 10:46:59 +0530

    Update Code_NodeMCU_Google_Alexa_SinricPro_4Relay_Switch.ino

--- Code_NodeMCU_Google_Alexa_SinricPro_4Relay_Switch.ino
+++ Code_NodeMCU_Google_Alexa_SinricPro_4Relay_Switch.ino
@@ -1,31 +1,3 @@
-/**********************************************************************************
- *  TITLE: Google + Alexa + Manual Switch/Button control 4 Relays using NodeMCU & Sinric Pro (Real time feedback)
- *  (flipSwitch can be a tactile button or a toggle switch) (code taken from Sinric Pro examples then modified)
- *  Click on the following links to learn more. 
- *  YouTube Video: https://youtu.be/gpB4600keWA
- *  Related Blog : https://iotcircuithub.com/esp8266-projects/
- *  by Tech StudyCell
- *  Preferences--> Aditional boards Manager URLs : 
- *  https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
- *  
- *  Download Board ESP8266 NodeMCU : https://github.com/esp8266/Arduino
- *  Download the libraries
- *  ArduinoJson Library: https://github.com/bblanchon/ArduinoJson
- *  arduinoWebSockets Library: https://github.com/Links2004/arduinoWebSockets
- *  SinricPro Library: https://sinricpro.github.io/esp8266-esp32-sdk/
- *  
- *  If you encounter any issues:
- * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
- * - ensure all dependent libraries are installed
- *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
- *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
- * - open serial monitor and check whats happening
- * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
- * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
- **********************************************************************************/
-
-// Uncomment the following line to enable serial debug output
-//#define ENABLE_DEBUG
 
 #ifdef ENABLE_DEBUG
        #define DEBUG_ESP_PORT Serial
@@ -64,9 +36,6 @@
 
 #define wifiLed   16   //D0
 
-// comment the following line if you use a toggle switches instead of tactile buttons
-//#define TACTILE_BUTTON 1
-
 #define BAUD_RATE   9600
 
 #define DEBOUNCE_TIME 250
@@ -77,10 +46,7 @@ typedef struct {      // struct for the std::map below
 } deviceConfig_t;
 
 
-// this is the main configuration
-// please put in your deviceId, the PIN for Relay and PIN for flipSwitch
-// this can be up to N devices...depending on how much pin's available on your device ;)
-// right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
+
 std::map<String, deviceConfig_t> devices = {
     //{deviceId, {relayPIN,  flipSwitchPIN}}
     {device_ID_1, {  RelayPin1, SwitchPin1 }},
@@ -95,8 +61,7 @@ typedef struct {      // struct for the std::map below
   unsigned long lastFlipSwitchChange;
 } flipSwitchConfig_t;
 
-std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
-                                                  // it will be setup in "setupFlipSwitches" function, using informations from devices map
+std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map 
 
 void setupRelays() { 
   for (auto &device : devices) {           // for each device (relay, flipSwitch combination)
